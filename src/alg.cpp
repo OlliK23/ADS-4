9527e8f6c463b360e65f2de6b3e937d599a4c6b3
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-24 19:28:02 +0300

    OOooooOO

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,54 @@
 // Copyright 2021 NNTU-CS
+int binfound(int *arr, int left, int right, int val) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == val) {
+            return mid;
+        } else if (arr[mid] < val) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return -1;
+}
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len-1; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    int l = 0;
+    int r = len - 1;
+    
+    while (l < r) {
+      if (arr[l] + arr[r] == value) {
+        count++;
+        l++;
+        r--;
+      } else if (arr[l] + arr[r] < value) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+    return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int count = 0;
+  for (int i = 0; i < len; i++) {
+    int index = binfound(arr, i + 1, len - 1, value - arr[i]);
+    if (index >= 0) {
+      count++;
+    }
+  }
+  return count;
 }
