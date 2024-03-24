c295c3a54af41829fa5be1a21400e4aa045dac4b
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-24 22:25:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,33 +1,44 @@
 // Copyright 2021 NNTU-CS
 #include <unistd.h>
-int binfound(int* arr, int left, int right, int val) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == val) {
-            return mid;
-        }else if (arr[mid] < val) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
+int binfound(int* arr, int size, int x) {
+    int l = 0, r = size - 1, count = 0;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x) {
+            count++;
+            int i = mid-1;
+            while (i >= 0 && arr[i] == x) {
+                count++;
+                i--;
+            }
+            i = mid +1;
+            while (i < size && arr[i] == x) {
+                count++;
+                i++;
+            }
+            return count;
+        } else if (arr[mid] < x) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
         }
     }
-    return -1;
+    return 0;
 }
+
 int countPairs1(int* arr, int len, int value) {
     sleep(10);
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len - 1; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
-        }
+  int count = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i+1; j < len; j++) {
+      if (arr[i] + arr[j] == value)
+        count++;
     }
-    return count;
+  }
+  return count;
 }
 int countPairs2(int* arr, int len, int value) {
-int count = 0;
+    int count = 0;
     int left = 0;
     int right = len - 1;
     while (arr[right] > value) {
@@ -45,11 +56,10 @@ int count = 0;
 
 int countPairs3(int* arr, int len, int value) {
     int count = 0;
+    int ind = 0;
     for (int i = 0; i < len; i++) {
-        int index = binfound(arr, i + 1, len - 1, value - arr[i]);
-        if (index >= 0) {
-            count++;
-        }
+        ind = value - arr[i];
+        count += binfound(&arr[i+1], len - i, x);
     }
     return count;
 }
