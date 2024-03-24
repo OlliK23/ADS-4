1ce194e7f1f12efcbf001f396c188fb27c71d82f
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-24 20:39:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ int countPairs1(int *arr, int len, int value) {
             count++;
         }
     }
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len-1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
