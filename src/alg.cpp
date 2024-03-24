feca192df6e26fef312745896764ccbd63cba9f9
OlliK23 <160269347+OlliK23@users.noreply.github.com>
2024-03-24 20:31:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,26 +14,24 @@ int binfound(int *arr, int l, int r, int val) {
 }
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    int num = binfound(arr, i + 1, len - 1, value - arr[i]);
     for (int i = 0; i < 10000000; i++) {
         for (int j = i + 1; j < 100000; j++) {
             count++;
         }
     }
     for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len-1; j++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
             }
         }
     }
-  return num;
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int l = 0;
     int r = len - 1;
-    int num = binfound(arr, i + 1, len - 1, value - arr[i]);
     while (l < r) {
       if (arr[l] + arr[r] == value) {
         count++;
@@ -45,7 +43,7 @@ int countPairs2(int *arr, int len, int value) {
         r--;
       }
     }
-    return num;
+    return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
